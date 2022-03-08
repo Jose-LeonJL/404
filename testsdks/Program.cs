e5e5784986989829fe4b33803ce6f84d8e62f138
unknown <dradefensasiciliana@gmail.com>
2022-03-08 17:42:55 -0600

    Login Complete

--- testsdks/Program.cs
+++ testsdks/Program.cs
@@ -6,8 +6,10 @@ namespace testsdks
     {
         static void Main(string[] args)
         {
-            Login.create_Login(new DVStudio.SDK.Estructuras.Struct_Login { Correo = "josemaleon44@gmail.com", Contraseña = "Password1" }).Wait();
-            Console.WriteLine("Hello World!");
+            //var result = Login.create_Login(new DVStudio.SDK.Estructuras.Struct_Login { Correo = "josemaleon44@gmail.com", Contraseña = "Password1" }).GetAwaiter().GetResult()
+            //var result2 = Login.Recuperar_Login(correo: "josemaleon44@gmail.com").GetAwaiter().GetResult();
+            //var result3 = Login.Validar_Codigo(new DVStudio.SDK.Estructuras.Validacion { code = "1599-9703", correo = ".josemaleon44@gmail.com" }).GetAwaiter().GetResult();
+            var result4 = Login.Actualizar_Contrasena(new DVStudio.SDK.Estructuras.Actualizar_contrasena { contraseña = "Password1", correo = "josemaleon44@gmail.com", id = "RpwAXlHuyypU4roP6326" }).GetAwaiter().GetResult();
         }
     }
 }
