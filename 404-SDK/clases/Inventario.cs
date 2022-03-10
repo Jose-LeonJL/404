6a3d28a92aec1c08ddf7c6d6732d13e2f405b040
unknown <dradefensasiciliana@gmail.com>
2022-03-10 15:55:36 -0600

    Avance

--- 404-SDK/clases/Inventario.cs
+++ 404-SDK/clases/Inventario.cs
@@ -6,29 +6,93 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using DVStudio.SDK.Exceptions;
+using DVStudio.SDK.Estructura_Respuesta;
 
 namespace DVStudio.SDK.clases
 {
     public class Inventario
     {
-        private string URL_base, access_token;
-        private HttpClient httpClient;
-        public Inventario(string URl_base, string access_token)
+        private const string URL_base = "https://api.dvstudio.dev";
+        private static HttpClient httpClient;
+        
+        public static async Task<Obtener_Inventario> Obtener_Inventario(string token)
         {
-            this.httpClient = new HttpClient(); 
-            this.URL_base = URl_base;
-            this.access_token = access_token;   
+            Obtener_Inventario Obtener_ = null;
+            try
+            {
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(URL_base);
+                httpClient.DefaultRequestHeaders.Add("x-access-token", token);
+                Console.WriteLine(JsonSerializer.Serialize(token));
+                StringContent body = new StringContent(JsonSerializer.Serialize(token), System.Text.Encoding.UTF8, "application/json");
+                var response = await httpClient.GetAsync("/Inventario");
+                var contenido = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(contenido);
+
+                Obtener_ = JsonSerializer.Deserialize<Obtener_Inventario>(contenido);
+                if (Obtener_.status == "error")
+                {
+                    Exception2 ex2 = JsonSerializer.Deserialize<Exception2>(contenido);
+                    ExceptionsResponse x2 = new ExceptionsResponse() { status = ex2.status, code = ex2.code, data = new ExceptionsResponse.Data { error = ex2.data.error } };
+                    Console.WriteLine(x2.status);
+                    throw x2;
+                }
+                else if (Obtener_ == null)
+                {
+                    throw new ExceptionsResponse() { status = "error", code = 404, data = new ExceptionsResponse.Data { error = "solicitud nula" } };
+                }
+                Console.WriteLine(Obtener_.status);
+            }
+            catch (ExceptionsResponse ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return Obtener_;
         }
-        public async Task create_Inventario(Struct_Inventario inventario)
+        public static async Task<Response_General> create_Inventario(Struct_Inventario inventario, string token)
         {
+            Response_General response_ = null;
+            try
+            {
+            httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(URL_base);
             //httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json; charset=utf-8");
-            httpClient.DefaultRequestHeaders.Add("x-access-token", access_token);
+            httpClient.DefaultRequestHeaders.Add("x-access-token", token);
             StringContent body = new StringContent(JsonSerializer.Serialize(inventario), System.Text.Encoding.UTF8,"application/json");
             var response = await httpClient.PostAsync("/Inventario", body);
             var contenido = await response.Content.ReadAsStringAsync();
             Console.WriteLine(contenido);
             Console.WriteLine(inventario.Marca);
+
+
+                response_ = JsonSerializer.Deserialize<Response_General>(contenido);
+                if (response_.status == "error")
+                {
+                    Exception2 ex2 = JsonSerializer.Deserialize<Exception2>(contenido);
+                    ExceptionsResponse x2 = new ExceptionsResponse() { status = ex2.status, code = ex2.code, data = new ExceptionsResponse.Data { error = ex2.data.error } };
+                    Console.WriteLine(x2.status);
+                    throw x2;
+                }
+                else if (response_ == null)
+                {
+                    throw new ExceptionsResponse() { status = "error", code = 404, data = new ExceptionsResponse.Data { error = "solicitud nula" } };
+                }
+                Console.WriteLine(response_.status);
+            }
+            catch (ExceptionsResponse ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return response_;
         }
     }
 }
--- testsdks/Program.cs
+++ testsdks/Program.cs
@@ -6,10 +6,26 @@ namespace testsdks
     {
         static void Main(string[] args)
         {
-            //var result = Login.create_Login(new DVStudio.SDK.Estructuras.Struct_Login { Correo = "josemaleon44@gmail.com", Contraseña = "Password1" }).GetAwaiter().GetResult()
+            var result = Login.create_Login(new DVStudio.SDK.Estructuras.Struct_Login { Correo = "josemaleon44@gmail.com", Contraseña = "Password1" }).GetAwaiter().GetResult();
             //var result2 = Login.Recuperar_Login(correo: "josemaleon44@gmail.com").GetAwaiter().GetResult();
             //var result3 = Login.Validar_Codigo(new DVStudio.SDK.Estructuras.Validacion { code = "1599-9703", correo = ".josemaleon44@gmail.com" }).GetAwaiter().GetResult();
-            var result4 = Login.Actualizar_Contrasena(new DVStudio.SDK.Estructuras.Actualizar_contrasena { contraseña = "Password1", correo = "josemaleon44@gmail.com", id = "RpwAXlHuyypU4roP6326" }).GetAwaiter().GetResult();
+            //var result4 = Login.Actualizar_Contrasena(new DVStudio.SDK.Estructuras.Actualizar_contrasena { contraseña = "Password1", correo = "josemaleon44@gmail.com", id = "RpwAXlHuyypU4roP6326" }).GetAwaiter().GetResult();
+            //var Obtene_inventario = Inventario.Obtener_Inventario(result.data.jwt).GetAwaiter().GetResult();
+            //Console.WriteLine(Obtene_inventario.code);
+            //foreach (var inventario in Obtene_inventario.data.products)
+            //{
+            //    Console.WriteLine(inventario.Data.Marca);
+            //}
+            var create_inv = Inventario.create_Inventario(new DVStudio.SDK.Estructuras.Struct_Inventario
+            {
+                Codigo = System.Guid.NewGuid().ToString(),
+                Categoria = System.Guid.NewGuid().ToString(),
+                Marca = System.Guid.NewGuid().ToString(),
+                Nombre = System.Guid.NewGuid().ToString(),
+                Precio = 1,
+                Stock = 1
+            }, result.data.jwt).GetAwaiter().GetResult();
+            Console.WriteLine(create_inv.data.id);
         }
     }
 }
