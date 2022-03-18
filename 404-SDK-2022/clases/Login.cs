55306ea760d656853831c17105f36980cd557392
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2022-03-18 17:34:08 -0600

    xd

---
+++ 404-SDK-2022/clases/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DVStudio.SDK.Estructuras;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DVStudio.SDK.clases
+{
+    public class Login
+    {
+        private const string URL_base = "https://api.dvstudio.dev";
+        private static HttpClient httpClient;
+        
+        public static async Task create_Login(Struct_Login Login)
+        {
+            try
+            {
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(URL_base);
+                Console.WriteLine(JsonSerializer.Serialize(Login));
+                StringContent body = new StringContent(JsonSerializer.Serialize(Login), System.Text.Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync("/Login", body);
+                var contenido = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JsonSerializer.Serialize(contenido));
+                //Console.WriteLine(Login.);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        public static void xd()
+        {
+
+        }
+    }
+}
