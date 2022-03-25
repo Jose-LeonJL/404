37b85fccc255f49a448137acf5ccf38d5d5119ae
Daniel Funez <darkfunezf@gmail.com>
2022-03-24 23:37:05 -0600

    Creacion de Validaciones de Campos Por Medio De Clases

--- 404-App/Clases_Validaciones/ClaseClientes.cs
+++ 404-App/Clases_Validaciones/ClaseClientes.cs
@@ -23,9 +23,9 @@ namespace _404_App.Clases_Validaciones
             //codigo
             RuleFor(x => x.Codigo).NotEmpty().WithMessage("El campo codigo no puede ir Nulo");
             //Identidad
-            RuleFor(x => x.Identidad).NotEmpty().WithMessage(" El Campo Identidad no puede ir nulo").MinimumLength(13).MaximumLength(13).WithMessage("tiene que tener 13 caracteres");
+            RuleFor(x => x.Identidad).NotEmpty().WithMessage(" El Campo Identidad no puede ir nulo").MinimumLength(15).MaximumLength(15).WithMessage("tiene que tener 15 caracteres con guiones ");
             //Telefono
-            RuleFor(x => x.Telefono).NotEmpty().WithMessage(" El Campo Telefono no puede ir nulo").MinimumLength(8).MaximumLength(8).WithMessage("tiene que tener 8 caracteres");
+            RuleFor(x => x.Telefono).NotEmpty().WithMessage(" El Campo Telefono no puede ir nulo").MinimumLength(9).MaximumLength(9).WithMessage("tiene que tener 9 caracteres con un guion en medio");
 
         }
     }
--- 404-App/Clases_Validaciones/ClaseUsuarios.cs
+++ 404-App/Clases_Validaciones/ClaseUsuarios.cs
@@ -35,11 +35,11 @@ namespace _404_App.Clases_Validaciones
             //Nombre
             RuleFor(x => x.Nombre).NotEmpty().WithMessage("El Campo Nombre no puede ir Nulo");
             //Identidad
-            RuleFor(x => x.Identidad).NotEmpty().WithMessage(" El Campo Identidad no puede ir nulo").MinimumLength(13).MaximumLength(13).WithMessage("tiene que tener 13 caracteres");
+            RuleFor(x => x.Identidad).NotEmpty().WithMessage(" El Campo Identidad no puede ir nulo").MinimumLength(15).MaximumLength(15).WithMessage("tiene que tener 15 caracteres con guiones");
             //Sueldo
             RuleFor(x => x.Sueldo).NotEmpty().WithMessage("El Campo Sueldo no puede ir Nulo");
             //Telefono
-            RuleFor(x => x.Telefono).NotEmpty().WithMessage(" El Campo Telefono no puede ir nulo").MinimumLength(8).MaximumLength(8).WithMessage("tiene que tener 8 caracteres");
+            RuleFor(x => x.Telefono).NotEmpty().WithMessage(" El Campo Telefono no puede ir nulo").MinimumLength(9).MaximumLength(9).WithMessage("tiene que tener 9 caracteres con un guion en medio");
             //Nick
             RuleFor(x => x.Nick).NotEmpty().WithMessage("El Campo Nick no puede ir Nulo");
             //Tipo
